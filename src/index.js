dad8b695701c06e5611d014debbec93a359d6313
java30433 <2159116373@qq.com>
2022-08-25 16:07:24 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,278 @@
+const { Extension, type, api } = require('clipcc-extension');
+const mp3 = require('./mp3');
+
+class MyExtension extends Extension {
+	constructor () {
+        super();
+	}
+    onInit() {
+		const runtime = api.getVmInstance().runtime;
+		const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+		api.addCategory({
+			categoryId: 'java30433.lazyload.category',
+			messageId: 'java30433.lazyload.category',
+			color: '#31A151'
+		});
+		const addBlock = function(blockJson){
+			let opcode = "java30433.lazyload.block." + blockJson.opcode;
+			api.addBlock({
+				opcode: opcode,
+				messageId: opcode,
+				categoryId: 'java30433.lazyload.category',
+				type: blockJson.type,
+				param: blockJson.param,
+				option: blockJson.option,
+				function: blockJson.function
+			});
+		};
+		//在项目停止时停止所有音乐的播放
+		runtime.on('PROJECT_STOP_ALL', ()=>{
+			Object.values(mp3.list).forEach(mp3ins=>{
+				mp3ins.pause();
+				mp3ins.time = s;
+			})
+		});
+		
+		addBlock({
+			opcode: 'loadMusic',
+			type: type.BlockType.COMMAND,
+			param: {
+                url: {
+                    type: type.ParameterType.STRING,
+                    default: 'https://api.codingclip.com/v1/project/asset/b4609e706e4d08f0cd7d220b5d2a634c.mp3'
+                },
+				name: {
+                    type: type.ParameterType.STRING,
+                    default: 'Name'
+                }
+			},
+			function: (args, util) => {
+				if (runtime.version.startsWith('c') && !args.url.startsWith('https://api.codingclip.com/v1/project/asset/')) return;
+				if (mp3.get(args.name) && mp3.get(args.name).url == args.url) return; //如果已经加载了一样的就不重复加载了
+				return mp3.pushAudio(args.name, args.url);
+			}
+		});
+		addBlock({
+			opcode: 'playMusic',
+			type: type.BlockType.COMMAND,
+			param: {
+				name: {
+                    type: type.ParameterType.STRING,
+                    default: 'Name'
+                },
+				n: {
+                    type: type.ParameterType.STRING,
+					field: true,
+                    menu: [{
+						messageId: 'java30433.lazyload.menu.loop',
+						value: 'loop'
+					}, {
+						messageId: 'java30433.lazyload.menu.unloop',
+						value: 'unloop'
+					}],
+					default: 'unloop'
+				},
+				rate: {
+					type: type.ParameterType.NUMBER,
+                    default: 'Name'
+				}
+			},
+			function: args => {
+				if (!mp3.get(args.name)) return;
+				const mp3ins = mp3.get(args.name);
+				mp3ins.loop = (args.n == 'loop');
+				mp3ins.rate = isNaN(args.rate) ? 1 : args.rate;
+				mp3ins.play();
+			}
+		});
+		addBlock({
+			opcode: 'pauseMusic',
+			type: type.BlockType.COMMAND,
+			param: {
+				name: {
+                    type: type.ParameterType.STRING,
+                    default: 'Name'
+                }
+			},
+			function: args => {
+				if (!mp3.get(args.name)) return;
+				mp3.get(args.name).pause();
+			}
+		});
+		addBlock({
+			opcode: 'deleteMusic',
+			type: type.BlockType.COMMAND,
+			param: {
+				name: {
+                    type: type.ParameterType.STRING,
+                    default: 'Name'
+                }
+			},
+			function: args => {
+				if (!mp3.get(args.name)) return;
+				mp3.get(args.name).pause();
+				delete mp3.list[args.name];
+			}
+		});
+		addBlock({
+			opcode: 'setMusicEffect',
+			type: type.BlockType.COMMAND,
+			param: {
+				n: {
+                    type: type.ParameterType.STRING,
+					field: true,
+                    menu: [{
+						messageId: 'java30433.lazyload.menu.currentTime',
+						value: 'currentTime'
+					}, {
+						messageId: 'java30433.lazyload.menu.volume',
+						value: 'volume'
+					}, {
+						messageId: 'java30433.lazyload.menu.panner',
+						value: 'panner'
+					}],
+					default: 'volume'
+				},
+                v: {
+                    type: type.ParameterType.NUMBER,
+                    default: 0
+                },
+				name: {
+                    type: type.ParameterType.STRING,
+                    default: 'Name'
+                }
+			},
+			function: args => {
+				if (!mp3.get(args.name)) return;
+				const mp3ins = mp3.get(args.name);
+				let v = isNaN(args.v) ? 0 : args.v;
+				switch (args.n) {
+					case 'currentTime':
+						mp3ins.setTime(v);
+						break;
+					case 'volume':
+						mp3ins.setVolume(v);
+						break;
+					case 'panner':
+						mp3ins.setPanner(v);
+						break;
+				}
+			}
+		});
+		addBlock({
+			opcode: 'getMusicState',
+			type: type.BlockType.REPORTER,
+			param: {
+				n: {
+                    type: type.ParameterType.STRING,
+					field: true,
+                    menu: [{
+						messageId: 'java30433.lazyload.menu.currentTime',
+						value: 'currentTime'
+					}, {
+						messageId: 'java30433.lazyload.menu.duration',
+						value: 'duration'
+					}, {
+						messageId: 'java30433.lazyload.menu.volume',
+						value: 'volume'
+					}, {
+						messageId: 'java30433.lazyload.menu.rate',
+						value: 'rate'
+					}, {
+						messageId: 'java30433.lazyload.menu.panner',
+						value: 'panner'
+					}],
+					default: 'currentTime'
+				},
+				name: {
+                    type: type.ParameterType.STRING,
+                    default: 'Name'
+                }
+			},
+			function: args => {
+				if (!mp3.get(args.name)) return "Error: Can't find audio";
+				const mp3ins = mp3.get(args.name);
+				switch (args.n) {
+					case 'currentTime':
+						return mp3ins.getNow();
+					case 'duration':
+						return mp3ins.duration;
+					case 'volume':
+						return mp3ins.getVolume();
+					case 'rate':
+						return mp3ins.rate;
+					case 'panner':
+						return mp3ins.getPanner();
+				}
+			}
+		});
+		addBlock({
+			opcode: 'getMusicStateBool',
+			type: type.BlockType.BOOLEAN,
+			param: {
+				n: {
+                    type: type.ParameterType.STRING,
+					field: true,
+                    menu: [{
+						messageId: 'java30433.lazyload.menu.ended',
+						value: 'ended'
+					}, {
+						messageId: 'java30433.lazyload.menu.paused',
+						value: 'playing'
+					}, {
+						messageId: 'java30433.lazyload.menu.loaded',
+						value: 'loaded'
+					}, {
+						messageId: 'java30433.lazyload.menu.loop',
+						value: 'loop'
+					}],
+					default: 'loaded'
+				},
+				name: {
+                    type: type.ParameterType.STRING,
+                    default: 'Name'
+                }
+			},
+			function: args => {
+				if (!mp3.get(args.name)) return false;
+				const mp3ins = mp3.get(args.name);
+				console.info(mp3ins);
+				switch (args.n) {
+					case 'ended':
+						return mp3ins.getNow()>=mp3ins.duration;
+					case 'playing':
+						return mp3ins.isPlaying;
+					case 'loaded':
+						return true; //在第一行判断过了，没就false
+					case 'loop':
+						return mp3ins.loop;
+				}
+			}
+		});
+		addBlock({
+			opcode: 'setRate',
+			type: type.BlockType.COMMAND,
+			param: {
+				name: {
+                    type: type.ParameterType.STRING,
+                    default: 'Name'
+                },
+				rate: {
+					type: type.ParameterType.NUMBER,
+                    default: 1
+				}
+			},
+			function: args => {
+				if (!mp3.get(args.name)) return;
+				const mp3ins = mp3.get(args.name);
+				mp3ins.setRate(isNaN(args.rate) ? 1 : args.rate);
+			},
+			option: {
+				//隐藏积木 播放过程中设置播放倍速会导致getNow()函数的偏差
+				filter: type.FilterType.HIDE
+			}
+		});
+	}
+}
+
+module.exports = MyExtension;
---
+++ src/mp3.js
@@ -0,0 +1,119 @@
+const mp3 = {};
+
+mp3.audioContext = new AudioContext();
+mp3.list = {}; //用于存储已加载音频
+
+mp3.pushAudio = function(name, url) {
+	const ctx = mp3.audioContext;
+	const request = new XMLHttpRequest();
+	return new Promise((resolve, reject) => {
+		request.open('GET', url, true);
+		request.responseType = 'arraybuffer';
+		request.onload = () => {
+			ctx.decodeAudioData(request.response, buffer => {
+				//下面开始初始化mp3Instance
+				mp3.list[name] = {};
+				const mp3ins = mp3.list[name];
+				mp3ins.src = url;
+				mp3ins.buffer = buffer;
+				mp3ins.time = 0;
+				mp3ins.startTime = ctx.currentTime;
+				mp3ins.stopTime = ctx.currentTime;
+				mp3ins.duration = mp3ins.buffer.duration;
+				//初始化rate
+				mp3ins.rate = 1;
+				//初始化音量节点
+				mp3ins.gainNode = ctx.createGain();
+				mp3ins.gainNode.gain.value = 1;
+				//初始化左右平衡节点
+				mp3ins.pannerNode = ctx.createPanner();
+				
+				
+				//获取当前播放时长
+				mp3ins.getNow = function() {
+					if (mp3ins.isPlaying) {
+						return (ctx.currentTime - mp3ins.startTime)*mp3ins.rate + mp3ins.time;
+					} else {
+						return mp3ins.time;
+					}
+				}
+
+				//开始播放
+				mp3ins.play = function() {
+					if(mp3ins.isPlaying) return;
+					//初始化buffSource
+					mp3ins.source = ctx.createBufferSource();
+					mp3ins.source.buffer = mp3ins.buffer;
+					mp3ins.source.loop = mp3ins.loop; //应用循环
+					mp3ins.source.playbackRate.value = mp3ins.rate; //应用播放倍率
+					//设置结束时的操作
+					mp3ins.source.onended = function() {
+						mp3ins.isPlaying = false;
+						mp3ins.stopTime = ctx.currentTime;
+						mp3ins.time = (ctx.currentTime - mp3ins.startTime)*mp3ins.rate + mp3ins.time;
+					};
+					
+					mp3ins.source.connect(mp3ins.gainNode); //链接音量节点
+					mp3ins.gainNode.connect(mp3ins.pannerNode); //链接平衡节点
+					mp3ins.pannerNode.connect(ctx.destination); //链接输出
+					
+					mp3ins.source.start(0, mp3ins.time); //播放
+					mp3ins.isPlaying = true;
+					mp3ins.startTime = ctx.currentTime;
+				};
+				//暂停
+				mp3ins.pause = function() {
+					if(mp3ins.source) mp3ins.source.stop(); //停止
+				}
+				//设置当前播放进度 TODO
+				mp3ins.setTime = function(s) {
+					try {
+						if(mp3ins.isPlaying) {
+							mp3ins.source.onended = function(){};
+							mp3ins.source.stop();
+							mp3ins.stopTime = ctx.currentTime;
+							mp3ins.time = s;
+							mp3ins.isPlaying = false;
+							mp3ins.play();
+						} else {
+							mp3ins.time = s;
+						}
+					} catch(e) {} //No problem
+				}
+				
+				//设置音乐音量 0~1
+				mp3ins.setVolume = function(volume) {
+					mp3ins.gainNode.gain.value = volume;
+				}
+				mp3ins.getVolume = function(){
+					return mp3ins.gainNode.gain.value;
+				}
+				
+				//设置rate 动态更改可行，但会导致getNow无法获取准确时间 TODO
+				mp3ins.setRate = function(rate) {
+					mp3ins.rate = rate;
+					if (mp3ins.isPlaying) mp3ins.source.playbackRate.value = rate;
+				}
+				
+				//设置左右平衡
+				mp3ins.setPanner = function(v) {
+					mp3ins.pannerNode.positionX.value = v; //这玩意其实大有搞头，不过我懒得写了。X方向的调节应该够用了
+				}
+				mp3ins.getPanner = function() {
+					return mp3ins.pannerNode.positionX;
+				}
+				
+				resolve(); //OVER!
+			});
+		};
+		request.onerror = error => reject(error);
+		request.send();
+	});
+};
+mp3.get = function(name) {
+	return mp3.list[name];
+}
+
+module.exports = mp3;
+//const buffer = await _getBuffer(URL);
+//buffer && playAudio(buffer);
